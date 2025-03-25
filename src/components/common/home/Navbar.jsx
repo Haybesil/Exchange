ba4e83f47b2df9b6e470fd7da44c479eb56a87b8
Silas Abayomi <abayomisilas50@gmail.com>
2025-03-25 17:09:16 +0100

    navbar update

--- src/components/common/home/Navbar.jsx
+++ src/components/common/home/Navbar.jsx
@@ -4,8 +4,8 @@ import { ButtonSolid } from '../base/buttons/Button'
 export default function Navbar() {
   return (
     <div>
-      <nav className='flex gap-10 lg:justify-between items-center pt-5'>
-        <img src="/images/logo2.png" alt="" className='h-9'/>
+      <nav className='flex justify-between items-center pt-5'>
+        <img src="/images/logo2.png" alt="" className='h-6 lg:h-9'/>
         <ButtonSolid className='!bg-[#00529e] !rounded-[50px]'>Chat Us</ButtonSolid>
       </nav>
     </div>
