ef0f968f325b927410829bb3b28966262302da36
Silas Abayomi <abayomisilas50@gmail.com>
2025-03-25 17:01:46 +0100

    Update Hero.jsx

--- src/components/common/home/Hero.jsx
+++ src/components/common/home/Hero.jsx
@@ -21,20 +21,30 @@ export default function Hero() {
             Trade with us on:
           </p>
           <div className="flex gap-4 flex-wrap">
-            <a
-              href="https://wa.me/2348139915048"
-              className={`${linkStyle} bg-[#35bb49]`}
-            >
-              <span>Whatsapp</span>
-              <img src="/images/whatsapp.svg" alt="whatsapp" className="h-6" />
-            </a>
-            <a
-              href="https://wa.me/2348139915048"
-              className={`${linkStyle} bg-[#cd377d]`}
-            >
-              <span>Instagram</span>
-              <img src="/images/instagram.svg" alt="whatsapp" className="h-6" />
-            </a>
+            <div className='flex gap-4'>
+              <a
+                href="https://wa.me/2348139915048"
+                className={`${linkStyle} bg-[#35bb49]`}
+              >
+                <span>Whatsapp</span>
+                <img
+                  src="/images/whatsapp.svg"
+                  alt="whatsapp"
+                  className="h-6"
+                />
+              </a>
+              <a
+                href="https://wa.me/2348139915048"
+                className={`${linkStyle} bg-[#cd377d]`}
+              >
+                <span>Instagram</span>
+                <img
+                  src="/images/instagram.svg"
+                  alt="whatsapp"
+                  className="h-6"
+                />
+              </a>
+            </div>
             <a
               href="https://wa.me/2348139915048"
               className={`${linkStyle} bg-[#00a8eb]`}
@@ -45,15 +55,15 @@ export default function Hero() {
           </div>
         </div>
         <div className="hero_cards flex lg:relative lg:left-10 h-[15rem] lg:h-[18rem] w-[25rem] lg:w-[30rem]">
-            <div className="float-item amazon">
-                <img src="/images/amazon.svg" alt="amazon" className="float-img"/>
-            </div>
-            <div className="float-item apple w-[100%] lg:w-[40%]">
-                <img src="/images/apple.svg" alt="apple" className="float-img"/>
-            </div>
-            <div className="float-item steam w-[100%] lg:w-[40%]">
-                <img src="/images/steam.svg" alt="steam" className="float-img"/>
-            </div>
+          <div className="float-item amazon">
+            <img src="/images/amazon.svg" alt="amazon" className="float-img" />
+          </div>
+          <div className="float-item apple w-[100%] lg:w-[40%]">
+            <img src="/images/apple.svg" alt="apple" className="float-img" />
+          </div>
+          <div className="float-item steam w-[100%] lg:w-[40%]">
+            <img src="/images/steam.svg" alt="steam" className="float-img" />
+          </div>
         </div>
       </div>
     </div>
